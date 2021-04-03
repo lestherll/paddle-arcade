19ce67aa75e294a7350ac6eed66e22166ebe4dd1
lestherll <qweqwe123.aa690@gmail.com>
2021-04-03 16:07:15 +0100

    Player class inherits from paddle w/ score field

--- pong/player.py
+++ pong/player.py
@@ -1,5 +1,18 @@
-class Player:
-    
-    
-    def __init__(self) -> None:
-        ...
+import arcade
+
+from pong.constants import PADDLE_HEIGHT, PADDLE_WIDTH
+from pong.game_objects.paddle import Paddle
+
+
+class Player(Paddle):
+
+
+    def __init__(self, width: int=PADDLE_WIDTH, height: int=PADDLE_HEIGHT, color=arcade.color.WHITE):
+        super().__init__(width, height, color)
+        self.__score = 0
+
+    def add_score(self):
+        self.__score += 1
+
+    def get_score(self):
+        return self.__score
